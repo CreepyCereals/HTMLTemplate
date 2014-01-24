94a1fd16bda412dfe1e1f47c61fa8d09e8c617a9
unknown <creepycereals@gmail.com>
2014-01-24 17:12:33 +0100

    Repo init

---
+++ plugin.py
@@ -0,0 +1,6 @@
+import sublime, sublime_plugin
+
+class HtmltemplateCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		template = '<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<meta http-equiv="Content-Type" content="text/html; charset=utf-8">\n\t\t<meta name="viewport" content="width=device-width, initial-scale=1.0" />\n\t\t<meta name="description" content="" />\n\t\t<title>Title</title>\n\t\t<link rel="icon" type="image/png" href="">\n\t\t<link rel="stylesheet" type="text/css" href="">\n\t\t<script type="text/javascript" src=""></script>\n\t</head>\n\t\t</body>\n\n\t\t</body>\n</html>"'
+		self.view.insert(edit, 0, template)
