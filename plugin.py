50cd284dcd9acc1a95c81077460952afd96c70ed
creepycereals <creepycereals@gmail.com>
2014-01-25 21:53:55 +0100

    Preferences working correctly

--- plugin.py
+++ plugin.py
@@ -1,7 +1,7 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, json
 
 class HtmltemplateCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.set_syntax_file('Packages/HTML/HTML.tmLanguage')
-		template = '<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<meta http-equiv="Content-Type" content="text/html; charset=utf-8">\n\t\t<meta name="viewport" content="width=device-width, initial-scale=1.0" />\n\t\t<meta name="description" content="" />\n\t\t<title>Title</title>\n\t\t<link rel="icon" type="image/png" href="">\n\t\t<link rel="stylesheet" type="text/css" href="">\n\t\t<script type="text/javascript" src=""></script>\n\t</head>\n\t\t<body>\n\n\t\t</body>\n</html>'
-		self.view.insert(edit, 0, template)
+		self.view.set_syntax_file('Packages/HTML/HTML.tmLanguage') # Sets the syntax to html
+		template = json.loads(open('preferences.json').read())['template'] # Loads the template saved in 'preferences.json'
+		self.view.insert(edit, 0, template) # Insertes the content of 'template'
