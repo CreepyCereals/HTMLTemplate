3de9322bcee1562f22cba7fef5a015a6674f63e6
creepycereals <creepycereals@gmail.com>
2014-01-25 21:32:38 +0100

    now the plugin also sets the file syntax to HTML

--- plugin.py
+++ plugin.py
@@ -2,5 +2,6 @@ import sublime, sublime_plugin
 
 class HtmltemplateCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		self.view.set_syntax_file('Packages/HTML/HTML.tmLanguage')
 		template = '<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<meta http-equiv="Content-Type" content="text/html; charset=utf-8">\n\t\t<meta name="viewport" content="width=device-width, initial-scale=1.0" />\n\t\t<meta name="description" content="" />\n\t\t<title>Title</title>\n\t\t<link rel="icon" type="image/png" href="">\n\t\t<link rel="stylesheet" type="text/css" href="">\n\t\t<script type="text/javascript" src=""></script>\n\t</head>\n\t\t<body>\n\n\t\t</body>\n</html>'
 		self.view.insert(edit, 0, template)
